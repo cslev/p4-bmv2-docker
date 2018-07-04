6be8d9be641236c7f75956e8a16dafd64b8958f6
cslev <cslev@gmx.com>
2018-07-04 15:21:36 +0200

    remove behavioral-modul

--- behavioral-model/hooks/README.md
+++
@@ -1,3 +0,0 @@
-Hooks for dockerhub build.
-See [docker
-documentation](https://docs.docker.com/docker-cloud/builds/advanced/#override-build-test-or-push-commands).
--- behavioral-model/travis/codecov.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-if [ "$GCOV" != "" ]; then
-   cd src/bm_sim
-   $GCOV -p -r -o .libs/ *.cpp
-   rm *third_party*
-   cd -
-   bash <(curl -s https://codecov.io/bash) -x $GCOV -p src/bm_sim
-fi
